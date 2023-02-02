abaae34dc7abe0daac8144ed2069596e97582565
Ayodeji <olusanyasamson633@gmail.com>
2023-02-03 00:06:30 +0100

    Whitelist-Contract

--- Whitelist-Tutorial/hardhat-tutorial/contracts/Whitelist.sol
+++ Whitelist-Tutorial/hardhat-tutorial/contracts/Whitelist.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.7;
 
-contract Name {
-    constructor() {}
+contract Whitelist {
+    // maximum number of addresses which can be whitelisted
+    uint8 public maxWhitelistedAddresses;
+
+    // keep track of number of addresses whitelisted till now
+    uint8 public numAddressesWhitelisted;
+
+    mapping(address => bool) whitelistedAddress;
+
+    constructor(uint8 _maxWhiteliestedAddresses) {
+        maxWhitelistedAddresses = _maxWhiteliestedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        require(
+            !whitelistedAddress[msg.sender],
+            "Sender already in the whitelist"
+        );
+        require(
+            numAddressesWhitelisted < maxWhitelistedAddresses,
+            "Max limit reached"
+        );
+        whitelistedAddress[msg.sender] = true;
+        numAddressesWhitelisted += 1;
+    }
 }
