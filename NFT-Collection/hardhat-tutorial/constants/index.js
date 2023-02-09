cb7a83e94d9c85499c0d252c2cea01636c2d4431
Ayodeji <olusanyasamson633@gmail.com>
2023-02-09 03:39:57 +0100

    correct-metadata

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -2,7 +2,7 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x665ebbd2a7b87a58cc873d472299181b5061afbc";
 // 0x665ebbd2a7b87a58cc873d472299181b5061afbc;
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://learn-web3-52a3ewc8b-ayodeji63.vercel.app/";
+const METADATA_URL = "https://learn-web3-dao-two.vercel.app/api/";
 //nft-collection-sneh1999.vercel.app/api/s
 
 https: module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
