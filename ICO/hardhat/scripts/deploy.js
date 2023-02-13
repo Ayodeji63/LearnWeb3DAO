f759d0553f5d1c909163ea969240a4e8e854fe25
Ayodeji <olusanyasamson633@gmail.com>
2023-02-13 12:03:17 +0100

    contract deployed

--- ICO/hardhat/scripts/deploy.js
+++ ICO/hardhat/scripts/deploy.js
@@ -1,10 +1,15 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
   const cryptoDevsTokenContract = await ethers.getContractFactory(
     "CryptoDevToken"
   );
+  const deployContract = await cryptoDevsTokenContract.deploy(
+    CRYPTO_DEVS_NFT_CONTRACT_ADDRESS
+  );
+  console.log("CryptoDev Token Contract Address:", deployContract.address);
 }
 
 main()
